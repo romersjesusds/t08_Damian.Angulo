bd85ea31a2b6e109c6030613abcbc09707de7120
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-11 22:47:19 -0500

    SE AGREGO LOS EJERCICIOS DE COMPARACION

---
+++ Damian/comparacion.py
@@ -0,0 +1,130 @@
+#ejercio 01
+
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase1="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+#       0         1         2         3         4         5
+#       0123456789012345678901234567890123456789012345678901234
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+print(frase1==frase2) #es falso
+print(frase1!=frase2) #es verdadero
+
+#ejercio 02
+
+#       0         1         2         3         4         5
+#       012345678901234567890123456789012345678901234567890123
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+print(frase2==frase3) #es falso
+print(frase2!=frase3) #es verdadero
+
+#ejercio 03
+
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+print(frase3==frase4) #es falso
+print(frase3!=frase4) #es verdadero
+
+#ejercio 04
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+print(frase4==frase5) #es falso
+print(frase4!=frase5) #es verdadero
+
+#ejercio 05
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+print(frase5==frase6) #es falso
+print(frase5!=frase6) #es verdadero
+
+#ejercio 06
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+#       0         1         2         3         4
+#       0123456789012345678901234567890123456789012345678
+frase7="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+print(frase6==frase7) #es falso
+print(frase6!=frase7) #es verdadero
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234578
+frase8="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOOS"
+print(frase7==frase8) #es falso
+print(frase7!=frase8) #es verdadero
+
+#ejercicio 9
+cad1="HIPOPOTAMO"
+cad2="MURCIELAGO"
+
+print(cad1==cad2) #es falso
+print(cad1!=cad2) #es verdadero
+
+#ejercicio 10
+cad3="BUENOS DIAS PROFESOR"
+cad4="BUENAS NOCHES PROFESOR"
+
+print(cad3==cad4) #es falso
+print(cad3!=cad4) #es verdadero
+
+#ejercicio 11
+cad5="SALI APROBADO"
+cad6="SALI DESAPROBADO"
+
+print(cad5==cad6) #es falso
+print(cad5!=cad6) #es verdadero
+
+#ejercicio 12
+cad7="TE AMO"
+cad8="TE QUIERO"
+
+print(cad7==cad8) #es falso
+print(cad7!=cad8) #es verdadero
+
+#ejercicio 13
+cad9="ELLA SI tE AMA"
+cad10="ELLA NO TE AMA"
+
+print(cad9==cad10) #es falso
+print(cad9!=cad10) #es verdadero
+
+#ejercicio 13
+cad11="COCER"
+cad12="COSER"
+
+print(cad11==cad12) #es falso
+print(cad11!=cad12) #es verdadero
+
+#ejercicio 14
+cad13="HIJO"
+cad14="HIGO"
+
+print(cad13==cad14) #es falso
+print(cad13!=cad14) #es verdadero
+
+#ejercicio 15
+cad15="BELLO"
+cad16="VELLO"
+
+print(cad15==cad16) #es falso
+print(cad15!=cad16) #es verdadero
+
+
