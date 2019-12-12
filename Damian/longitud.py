891cf156c6c64b1db1e3f34ec167d98489dadeab
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-11 22:48:22 -0500

    SE AGREGO LOS EJERCICIOS DE LONGITUD

---
+++ Damian/longitud.py
@@ -0,0 +1,107 @@
+#ejercio 01
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase1="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+longitud=len(frase1)
+print("la longitud de la cadena 'frase 1' es:", longitud)
+
+#ejercio 02
+#       0         1         2         3         4         5
+#       012345678901234567890123456789012345678901234567890123
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+longitud=len(frase2)
+print("la longitud de la cadena 'frase 2' es:", longitud)
+
+#ejercio 03
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+longitud=len(frase3)
+print("la longitud de la cadena 'frase 3' es:", longitud)
+
+#ejercio 04
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+longitud=len(frase4)
+print("la longitud de la cadena 'frase 4' es:", longitud)
+
+#ejercio 05
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+longitud=len(frase5)
+print("la longitud de la cadena 'frase 5' es:", longitud)
+
+#ejercio 06
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+longitud=len(frase6)
+print("la longitud de la cadena 'frase 6' es:", longitud)
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+
+longitud=len(frase7)
+print("la longitud de la cadena 'frase 7' es:", longitud)
+
+#ejercio 08
+#       0         1         2         3         4
+#       0123456789012345678901234567890123456789012345
+frase8="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOS"
+longitud=len(frase8)
+print("la longitud de la cadena 'frase 8' es:", longitud)
+
+#ejercio 09
+#       0         1         2         3         4         5         6
+#       0123456789012345678901234567890123456789012345678901234567890123456
+frase9="LAS TORMENTAS HACEN QUE LOS ARBOLES TENGAN LAS RAICES MAS PROFUNDAS"
+longitud=len(frase9)
+print("la longitud de la cadena 'frase 9' es:", longitud)
+
+#ejercio 10
+#        0         1         2         3         4
+#        0123456789012345678901234567890123456789012
+frase10="CUANDO ESTA OSCURO PUEDES VER LAS ESTRELLAS"
+longitud=len(frase10)
+print("la longitud de la cadena 'frase 10' es:", longitud)
+
+#ejercio 11
+#        0         1         2         3         4         5
+#        012345678901234567890123456789012345678901234567890
+frase11="LA UNICA DISCAPACIDAD EN LA VIDA ES LA MALA ACTITUD"
+longitud=len(frase11)
+print("la longitud de la cadena 'frase 11' es:", longitud)
+
+#ejercio 12
+#        0         1         2         3         4         5         6         7
+#        01234567890123456789012345678901234567890123456789012345678901234567890123
+frase12="TODO ES O UNA OPORTUNIDAD PARA CRECER O UN OBSTACULO QUE EVITA QUE CREZCAS"
+longitud=len(frase12)
+print("la longitud de la cadena 'frase 12' es:", longitud)
+
+#ejercio 13
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890
+frase13="LA VIDA ES MUY SIMPLE, PERO NOS EMPENIAMOS EN HACERLA DIFICIL"
+longitud=len(frase13)
+print("la longitud de la cadena 'frase 13' es:", longitud)
+
+#ejercio 14
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890123
+frase14="EL VERDADERO ROMANCE SURGE CUANDO APRENDES A QUERERTE A TI MISMO"
+longitud=len(frase14)
+print("la longitud de la cadena 'frase 14' es:", longitud)
+
+#ejercio 15
+#        0         1         2         3         4         5         6
+#        012345678901234567890123456789012345678901234567890123456789012345
+frase15="LAS PERSONAS MAS IMPORTANTES NO SE BUSCAN, LA VIDA TE LAS PRESENTA"
+longitud=len(frase15)
+print("la longitud de la cadena 'frase 15' es:", longitud)
+
+
