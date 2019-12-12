cbd03321a278d1a288029bf2ad62212e85c384a1
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-12 18:35:38 -0500

    SE AGREGO LOS EJERCICIOS DE busqueda

---
+++ Damian/iteracion.py
@@ -0,0 +1,105 @@
+#ejercio 01
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase1="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+for letra in frase1:
+    print(letra)
+
+#ejercio 02
+#       0         1         2         3         4         5
+#       012345678901234567890123456789012345678901234567890123
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+for letra in frase2:
+    print(letra)
+
+#ejercio 03
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+for letra in frase3:
+    print(letra)
+
+#ejercio 04
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+for letra in frase4:
+    print(letra)
+
+#ejercio 05
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+for letra in frase5:
+    print(letra)
+
+#ejercio 06
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+for letra in frase6:
+    print(letra)
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase7="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+for letra in frase7:
+    print(letra)
+
+#ejercio 08
+#       0         1         2         3         4
+#       0123456789012345678901234567890123456789012345
+frase8="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOS"
+for letra in frase8:
+    print(letra)
+
+#ejercio 09
+#       0         1         2         3         4         5         6
+#       0123456789012345678901234567890123456789012345678901234567890123456
+frase9="LAS TORMENTAS HACEN QUE LOS ARBOLES TENGAN LAS RAICES MAS PROFUNDAS"
+for letra in frase9:
+    print(letra)
+
+#ejercio 10
+#        0         1         2         3         4
+#        0123456789012345678901234567890123456789012
+frase10="CUANDO ESTA OSCURO PUEDES VER LAS ESTRELLAS"
+for letra in frase10:
+    print(letra)
+
+#ejercio 11
+#        0         1         2         3         4         5
+#        012345678901234567890123456789012345678901234567890
+frase11="LA UNICA DISCAPACIDAD EN LA VIDA ES LA MALA ACTITUD"
+for letra in frase11:
+    print(letra)
+
+#ejercio 12
+#        0         1         2         3         4         5         6         7
+#        01234567890123456789012345678901234567890123456789012345678901234567890123
+frase12="TODO ES O UNA OPORTUNIDAD PARA CRECER O UN OBSTACULO QUE EVITA QUE CREZCAS"
+for letra in frase12:
+    print(letra)
+
+#ejercio 13
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890
+frase13="LA VIDA ES MUY SIMPLE, PERO NOS EMPENIAMOS EN HACERLA DIFICIL"
+for letra in frase13:
+    print(letra)
+
+#ejercio 14
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890123
+frase14="EL VERDADERO ROMANCE SURGE CUANDO APRENDES A QUERERTE A TI MISMO"
+for letra in frase14:
+    print(letra)
+
+#ejercio 15
+#        0         1         2         3         4         5         6
+#        012345678901234567890123456789012345678901234567890123456789012345
+frase15="LAS PERSONAS MAS IMPORTANTES NO SE BUSCAN, LA VIDA TE LAS PRESENTA"
+for letra in frase15:
+    print(letra)
+
