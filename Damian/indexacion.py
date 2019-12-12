0141d99b29aeaca80548a18e72b763505e3ecd8d
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-11 19:23:21 -0500

    SE AGREGO LOS EJERCICIOS DE INDEXACION

---
+++ Damian/indexacion.py
@@ -0,0 +1,255 @@
+#ejercio 01
+#      0         1         2         3
+#      0123456789012345678901234567890123456789
+frase="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+
+print(frase[9])  #imprime la C
+print(frase[34]) #imprime la I
+print(frase[33]) #imprime la U
+print(frase[35]) #imprime la D
+print(frase[36]) #imprime la A
+print(frase[35]) #imprime la D
+print(frase[13]) #imprime la E
+print(frase[0])  #imprime la L
+print(frase[1])  #imprime la A
+
+#ejercio 02
+#      0         1         2         3         4         5
+#      012345678901234567890123456789012345678901234567890123
+frase="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+
+print(frase[3])  #imprime la M
+print(frase[4])  #imprime la E
+print(frase[6])  #imprime la T
+print(frase[7])  #imprime la E
+print(frase[15]) #imprime la O
+print(frase[22]) #imprime la R
+print(frase[26]) #imprime la I
+print(frase[6])  #imprime la T
+print(frase[15]) #imprime la O
+
+#ejercio 03
+#      0         1         2         3         4         5
+#      01234567890123456789012345678901234567890123456789012
+frase="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+
+print(frase[11]) #imprime la A
+print(frase[14]) #imprime la L
+print(frase[20]) #imprime la G
+print(frase[11]) #imprime la A
+print(frase[44]) #imprime la R
+print(frase[44]) #imprime la R
+print(frase[1])  #imprime la O
+print(frase[49]) #imprime la B
+print(frase[24]) #imprime la O
+print(frase[18]) #imprime la S
+
+#ejercio 04
+#      0         1         2         3         4
+#      012345678901234567890123456789012345678901234
+frase="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+
+print(frase[2])  #imprime la R
+print(frase[1])  #imprime la O
+print(frase[7])  #imprime la S
+print(frase[5])  #imprime la A
+print(frase[24]) #imprime la R
+print(frase[40]) #imprime la I
+print(frase[43]) #imprime la O
+
+#ejercio 05
+#      0         1         2         3
+#      0123456789012345678901234567890123456789
+frase="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+
+print(frase[10]) #imprime la M
+print(frase[2])  #imprime la A
+print(frase[24]) #imprime la R
+print(frase[38]) #imprime la I
+print(frase[5])  #imprime la O
+print(frase[26]) #imprime la N
+print(frase[27]) #imprime la E
+print(frase[7])  #imprime la T
+print(frase[34]) #imprime la A
+
+#ejercio 06
+#      0         1         2         3         4
+#      01234567890123456789012345678901234567890123456
+frase="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+
+print(frase[4])  #imprime la E
+print(frase[11]) #imprime la S
+print(frase[44]) #imprime la T
+print(frase[28]) #imprime la U
+print(frase[13]) #imprime la D
+print(frase[37]) #imprime la I
+print(frase[7])  #imprime la A
+print(frase[0])  #imprime la N
+print(frase[44]) #imprime la T
+print(frase[4])  #imprime la E
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+
+print(frase[0])  #imprime la C
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[34]) #imprime la A
+print(frase[36]) #imprime la R
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[31]) #imprime la A
+print(frase[21]) #imprime la S
+
+#ejercio 08
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345
+frase="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOS"
+
+print(frase[0])  #imprime la C
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[34]) #imprime la A
+print(frase[36]) #imprime la R
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[31]) #imprime la A
+print(frase[21]) #imprime la S
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+
+print(frase[0])  #imprime la C
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[34]) #imprime la A
+print(frase[36]) #imprime la R
+print(frase[4])  #imprime la A
+print(frase[33]) #imprime la T
+print(frase[31]) #imprime la A
+print(frase[21]) #imprime la S
+
+#ejercio 08
+#      0         1         2         3         4         5         6
+#      01234567890123456789012345678901234567890123456789012345678901
+frase="ella era un poeta en un mundo que todavia aprendia el alfabeto"
+
+print(frase[56])  #imprime la F
+print(frase[3])   #imprime la A
+print(frase[24])  #imprime la M
+print(frase[39])  #imprime la I
+print(frase[55])  #imprime la L
+print(frase[39])  #imprime la I
+print(frase[3])   #imprime la A
+
+print(frase[58]) #imprime la B
+print(frase[13]) #imprime la O
+print(frase[26]) #imprime la N
+print(frase[39]) #imprime la I
+print(frase[34]) #imprime la T
+print(frase[3])  #imprime la A
+
+#ejercio 09
+#      0         1         2         3         4         5         6
+#      0123456789012345678901234567890123456789012345678901234567890123456
+frase="LAS TORMENTAS HACEN QUE LOS ARBOLES TENGAN LAS RAICES MAS PROFUNDAS"
+
+print(frase[7])    #imprime la M
+print(frase[5])    #imprime la O
+print(frase[6])    #imprime la R
+print(frase[6])    #imprime la R
+print(frase[5])    #imprime la O
+print(frase[58])   #imprime la P
+print(frase[22])   #imprime la E
+
+#ejercio 10
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012
+frase="CUANDO ESTA OSCURO PUEDES VER LAS ESTRELLAS"
+
+print(frase[0])   #imprime la C
+print(frase[10])  #imprime la A
+print(frase[16])  #imprime la R
+print(frase[16])  #imprime la R
+print(frase[1])   #imprime la U
+print(frase[0])   #imprime la C
+print(frase[7])   #imprime la E
+print(frase[39])  #imprime la L
+
+#ejercio 11
+#      0         1         2         3         4         5
+#      012345678901234567890123456789012345678901234567890
+frase="LA UNICA DISCAPACIDAD EN LA VIDA ES LA MALA ACTITUD"
+
+print(frase[39])  #imprime la M
+print(frase[5])   #imprime la I
+print(frase[0])   #imprime la L
+print(frase[22])  #imprime la E
+print(frase[4])   #imprime la N
+print(frase[1])   #imprime la A
+
+#ejercio 12
+#      0         1         2         3         4         5         6         7
+#      01234567890123456789012345678901234567890123456789012345678901234567890123
+frase="TODO ES O UNA OPORTUNIDAD PARA CRECER O UN OBSTACULO QUE EVITA QUE CREZCAS"
+
+print(frase[12])  #imprime la A
+print(frase[17])  #imprime la R
+print(frase[18])  #imprime la T
+print(frase[59])  #imprime la I
+print(frase[48])  #imprime la C
+print(frase[49])  #imprime la U
+print(frase[50])  #imprime la L
+print(frase[51])  #imprime la O
+
+#ejercio 13
+#      0         1         2         3         4         5         6
+#      0123456789012345678901234567890123456789012345678901234567890
+frase="LA VIDA ES MUY SIMPLE, PERO NOS EMPENIAMOS EN HACERLA DIFICIL"
+
+print(frase[3])   #imprime la V
+print(frase[6])   #imprime la A
+print(frase[36])  #imprime la N
+print(frase[4])   #imprime la I
+print(frase[5])   #imprime la D
+print(frase[6])   #imprime la A
+print(frase[5])   #imprime la D
+
+#ejercio 14
+#      0         1         2         3         4         5         6
+#      0123456789012345678901234567890123456789012345678901234567890123
+frase="EL VERDADERO ROMANCE SURGE CUANDO APRENDES A QUERERTE A TI MISMO"
+
+print(frase[35])   #imprime la P
+print(frase[11])   #imprime la O
+print(frase[17])   #imprime la N
+print(frase[18])   #imprime la C
+print(frase[57])   #imprime la I
+print(frase[32])   #imprime la O
+
+print(frase[35])   #imprime la P
+print(frase[57])   #imprime la I
+print(frase[1])    #imprime la L
+print(frase[7])    #imprime la A
+print(frase[51])   #imprime la T
+print(frase[14])   #imprime la O
+
+#ejercio 15
+#      0         1         2         3         4         5         6
+#      012345678901234567890123456789012345678901234567890123456789012345
+frase="LAS PERSONAS MAS IMPORTANTES NO SE BUSCAN, LA VIDA TE LAS PRESENTA"
+
+print(frase[38])  #imprime la C
+print(frase[36])   #imprime la U
+print(frase[1])  #imprime la A
+print(frase[48])  #imprime la D
+print(frase[5])  #imprime la E
+print(frase[6])  #imprime la R
+print(frase[40])   #imprime la N
+print(frase[17])  #imprime la I
+print(frase[22])  #imprime la T
+print(frase[20])   #imprime la O
