b0a14062e35c8012cb569cff2ffd46b2a9ffcc0d
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-12 18:35:10 -0500

    SE AGREGO LOS EJERCICIOS DE busqueda

---
+++ Damian/cortado.py
@@ -0,0 +1,124 @@
+#ejercio 01
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase1="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+print(frase1[9:14]) #resultado => calle
+print(frase1[32:38])  #resultado => cuidar
+print(frase1[21:27])  #resultado => enseña
+print(frase1[:2])      #resultado => la
+
+#ejercio 02
+#       0         1         2         3         4         5
+#       012345678901234567890123456789012345678901234567890123
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+print(frase1[3:8]) #resultado => calle
+print(frase1[20:30])  #resultado => cuidar
+print(frase1[21:27])  #resultado => enseña
+print(frase1[:2])      #resultado => la
+
+#ejercio 03
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+print(frase3[6:12])   #resultado => CUENTA
+print(frase3[18:26])  #resultado => SEGUNDOS
+print(frase3[39:48])  #resultado => HISTORIAS
+
+
+
+#ejercio 04
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+print(frase4[7:14]) #resultado => SONRISA
+print(frase4[21:26])  #resultado => MADRE
+print(frase4[39:])  #resultado => MILLON
+
+#ejercio 05
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+print(frase5[10:15]) #resultado => MUNDO
+print(frase5[19:28])  #resultado => DESMORONE
+print(frase5[37:])  #resultado => MIO
+
+#ejercio 06
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+print(frase6[16:21]) #resultado => CALLE
+print(frase6[27:32])  #resultado => SUELA
+print(frase6[40:])  #resultado => ZAPATOS
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase7="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+print(frase7[:6])     #resultado => CUIDADO
+print(frase7[16:21])  #resultado => MIEDOS
+print(frase7[36:41])  #resultado => ROBAR
+
+#ejercio 08
+#       0         1         2         3         4
+#       0123456789012345678901234567890123456789012345
+frase8="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOS"
+print(frase8[26:33]) #resultado => CAMBIAR
+print(frase8[:5])  #resultado => SOMOS
+print(frase8[21:25])  #resultado => PARA
+
+#ejercio 09
+#       0         1         2         3         4         5         6
+#       0123456789012345678901234567890123456789012345678901234567890123456
+frase9="LAS TORMENTAS HACEN QUE LOS ARBOLES TENGAN LAS RAICES MAS PROFUNDAS"
+print(frase9[4:13]) #resultado => TORMENTAS
+print(frase9[28:35])  #resultado => ARBOLES
+print(frase9[47:53])  #resultado => RAICES
+
+#ejercio 10
+#        0         1         2         3         4
+#        0123456789012345678901234567890123456789012
+frase10="CUANDO ESTA OSCURO PUEDES VER LAS ESTRELLAS"
+print(frase10[:6]) #resultado => CUANDO
+print(frase10[12:18])  #resultado => OSCURO
+print(frase10[34:])  #resultado => ESTRELLAS
+
+#ejercio 11
+#        0         1         2         3         4         5
+#        012345678901234567890123456789012345678901234567890
+frase11="LA UNICA DISCAPACIDAD EN LA VIDA ES LA MALA ACTITUD"
+print(frase11[3:8]) #resultado => UNICA
+print(frase11[9:21])  #resultado => DISCAPACIDAD
+print(frase11[28:32])  #resultado => VIDA
+
+#ejercio 12
+#        0         1         2         3         4         5         6         7
+#        01234567890123456789012345678901234567890123456789012345678901234567890123
+frase12="TODO ES O UNA OPORTUNIDAD PARA CRECER O UN OBSTACULO QUE EVITA QUE CREZCAS"
+print(frase12[14:25]) #resultado => OPORTUNIDAD
+print(frase12[31:37])  #resultado => CRECER
+print(frase12[43:52])  #resultado => OBSTACULO
+
+#ejercio 13
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890
+frase13="LA VIDA ES MUY SIMPLE, PERO NOS EMPENIAMOS EN HACERLA DIFICIL"
+print(frase13[15:21]) #resultado => SIMPLE
+print(frase13[46:53])  #resultado => HACERLA
+print(frase13[54:])  #resultado => DIFICIL
+
+#ejercio 14
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890123
+frase14="EL VERDADERO ROMANCE SURGE CUANDO APRENDES A QUERERTE A TI MISMO"
+print(frase14[3:12]) #resultado => VERDADERO
+print(frase14[13:20])  #resultado => ROMANCE
+print(frase14[45:53])  #resultado => QUERERTE
+
+#ejercio 15
+#        0         1         2         3         4         5         6
+#        012345678901234567890123456789012345678901234567890123456789012345
+frase15="LAS PERSONAS MAS IMPORTANTES NO SE BUSCAN, LA VIDA TE LAS PRESENTA"
+print(frase15[4:12]) #resultado =>PERSONAS
+print(frase15[17:28])  #resultado => IMPORTANTES
+print(frase15[35:41])  #resultado => BUSCAN
+
