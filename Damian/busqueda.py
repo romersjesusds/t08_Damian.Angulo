eb0cfa17a34ebe0a07efa17b75ee7dd03a1ded38
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-12 18:34:39 -0500

    SE AGREGO LOS EJERCICIOS DE busqueda

---
+++ Damian/busqueda.py
@@ -0,0 +1,122 @@
+#ejercio 01
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase1="LA MISMA CALLE ME HA ENSEÑADO A CUIDARME"
+print(frase1.find("CALLE")) #SE UBICA EN 9
+print(frase1.index("ENSEÑADO")) #SE UBICA EN 21
+print(frase1.index("CUIDARME")) #SE UBICA EN 32
+
+#ejercio 02
+#       0         1         2         3         4         5
+#       012345678901234567890123456789012345678901234567890123
+frase2="LA MENTE ES COMO UN PARACAIDAS, NO SIRVE SI NO SE ABRE"
+print(frase2.find("COMO")) #SE UBICA EN 12
+print(frase2.index("PARACAIDAS")) #SE UBICA EN 20
+print(frase2.index("SIRVE")) #SE UBICA EN 35
+
+#ejercio 03
+#       0         1         2         3         4         5
+#       01234567890123456789012345678901234567890123456789012
+frase3="NO SE CUENTAN LOS SEGUNDOS, SE CUENTAN HISTORIAS BABY"
+print(frase3.find("CUENTAN")) #SE UBICA EN 6
+print(frase3.index("SEGUNDOS")) #SE UBICA EN 18
+print(frase3.index("HISTORIAS")) #SE UBICA EN 39
+
+#ejercio 04
+#       0         1         2         3         4
+#       012345678901234567890123456789012345678901234
+frase4="POR LA SONRISA DE MI MADRE QUE VALE UN MILLON"
+print(frase4.find("SONRISA")) #SE UBICA EN 7
+print(frase4.index("MADRE")) #SE UBICA EN 21
+print(frase4.index("MI")) #SE UBICA EN 18
+
+#ejercio 05
+#       0         1         2         3
+#       0123456789012345678901234567890123456789
+frase5="CUANDO TU MUNDO SE DESMORONE, VEN AL MIO"
+print(frase5.find("MUNDO")) #SE UBICA EN 10
+print(frase5.index("DESMORONE")) #SE UBICA EN 19
+print(frase5.index("VEN")) #SE UBICA EN 30
+
+
+#ejercio 06
+#       0         1         2         3         4
+#       01234567890123456789012345678901234567890123456
+frase6="NO LE HABLES DE CALLE A LA SUELA DE MIS ZAPATOS"
+print(frase6.find("CALLE")) #SE UBICA EN 16
+print(frase6.index("HABLES")) #SE UBICA EN 6
+print(frase6.index("ZAPATOS")) #SE UBICA EN 40
+
+#ejercio 07
+#      0         1         2         3         4
+#      0123456789012345678901234567890123456789012345678
+frase7="CUIDADO CON LOS MIEDOS, LES ENCANTA ROBAR SUENIOS"
+print(frase7.find("LOS")) #SE UBICA EN 13
+print(frase7.index("ROBAR")) #SE UBICA EN 37
+print(frase7.index("ENCANTA")) #SE UBICA EN 29
+
+#ejercio 08
+#       0         1         2         3         4
+#       0123456789012345678901234567890123456789012345
+frase8="SOMOS LO QUE HACEMOS PARA CAMBIAR LO QUE SOMOS"
+print(frase8.find("QUE")) #SE UBICA EN
+print(frase8.index("LO")) #SE UBICA EN
+print(frase8.index("SOMOS")) #SE UBICA EN
+
+#ejercio 09
+#       0         1         2         3         4         5         6
+#       0123456789012345678901234567890123456789012345678901234567890123456
+frase9="LAS TORMENTAS HACEN QUE LOS ARBOLES TENGAN LAS RAICES MAS PROFUNDAS"
+print(frase9.find("HACEN")) #SE UBICA EN 14
+print(frase9.index("LOS")) #SE UBICA EN 24
+print(frase9.index("MAS")) #SE UBICA EN 54
+
+#ejercio 10
+#        0         1         2         3         4
+#        0123456789012345678901234567890123456789012
+frase10="CUANDO ESTA OSCURO PUEDES VER LAS ESTRELLAS"
+print(frase10.find("ESTA")) #SE UBICA EN 7
+print(frase10.index("OSCURO")) #SE UBICA EN 12
+print(frase10.index("VER")) #SE UBICA EN 26
+
+#ejercio 11
+#        0         1         2         3         4         5
+#        012345678901234567890123456789012345678901234567890
+frase11="LA UNICA DISCAPACIDAD EN LA VIDA ES LA MALA ACTITUD"
+print(frase11.find("UNICA")) #SE UBICA EN 3
+print(frase11.index("EN")) #SE UBICA EN 22
+print(frase11.index("VIDA")) #SE UBICA EN 28
+
+#ejercio 12
+#        0         1         2         3         4         5         6         7
+#        01234567890123456789012345678901234567890123456789012345678901234567890123
+frase12="TODO ES O UNA OPORTUNIDAD PARA CRECER O UN OBSTACULO QUE EVITA QUE CREZCAS"
+print(frase12.find("UNA")) #SE UBICA EN 10
+print(frase12.index("PARA")) #SE UBICA EN 26
+print(frase12.index("EVITA")) #SE UBICA EN 57
+
+#ejercio 13
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890
+frase13="LA VIDA ES MUY SIMPLE, PERO NOS EMPENIAMOS EN HACERLA DIFICIL"
+print(frase13.find("MUY")) #SE UBICA EN 11
+print(frase13.index("SIMPLE")) #SE UBICA EN 15
+print(frase13.index("PERO")) #SE UBICA EN 23
+
+#ejercio 14
+#        0         1         2         3         4         5         6
+#        0123456789012345678901234567890123456789012345678901234567890123
+frase14="EL VERDADERO ROMANCE SURGE CUANDO APRENDES A QUERERTE A TI MISMO"
+print(frase14.find("EL")) #SE UBICA EN 0
+print(frase14.index("ROMANCE")) #SE UBICA EN 13
+print(frase14.index("CUANDO")) #SE UBICA EN 27
+
+#ejercio 15
+#        0         1         2         3         4         5         6
+#        012345678901234567890123456789012345678901234567890123456789012345
+frase15="LAS PERSONAS MAS IMPORTANTES NO SE BUSCAN, LA VIDA TE LAS PRESENTA"
+print(frase15.find("MAS")) #SE UBICA EN 13
+print(frase15.index("PERSONAS")) #SE UBICA EN 4
+print(frase15.index("BUSCAN")) #SE UBICA EN 35
+
+
