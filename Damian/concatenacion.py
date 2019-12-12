33a4729f630af5d78477956bb594128d5bdc0d1a
romersjesusds <56905465+romersjesusds@users.noreply.github.com>
2019-12-11 23:50:57 -0500

    SE AGREGO LOS EJERCICIOS DE concatenacion

---
+++ Damian/concatenacion.py
@@ -0,0 +1,89 @@
+#ejercicio 1
+nombres="ROMERS JESUS"
+apellidos="DAMIAN SUCLUPE"
+
+print("Mi nombre es: "+nombres+" "+apellidos)
+
+#ejercicio 2
+tipo_universidad="Nacional"
+nomb_universidad="'PEDRO RUIZ GALLO'"
+
+print("Mi universidad se llama "+nomb_universidad+" y es "+tipo_universidad)
+
+#ejercicio 3
+curso="Programacion"
+horas=5
+
+print("Cada semana tengo "+str(horas)+" horas "+"del curso de "+curso)
+
+#ejercicio 4
+talento1="bailar"
+talento2="programar"
+h=" "
+print("Mis mayores talentos son: "+talento1+"\n"+26*h+talento2)
+
+#ejercicio 5
+palabra_esp="'Perro'"
+ingles="'Dog'"
+
+print("El ingles de la palabra "+palabra_esp+" es "+ingles)
+
+#ejercicio 6
+palabra_esp2="'Gato'"
+ingles2="'Cat'"
+
+print("El ingles de la palabra "+palabra_esp2+" es "+ingles2)
+
+#ejercicio 7
+palabra_esp3="'Burro'"
+ingles3="'Butter'"
+
+print("El ingles de la palabra "+palabra_esp3+" es "+ingles3)
+
+#ejercicio 8
+palabra_esp4="'caballo'"
+ingles4="'horse'"
+
+print("El ingles de la palabra "+palabra_esp4+" es "+ingles4)
+
+#ejercicio 9
+palabra_esp5="'Maestro'"
+ingles5="'Master'"
+
+print("El ingles de la palabra "+palabra_esp5+" es "+ingles5)
+
+#ejercicio 10
+palabra_esp6="'Mesa'"
+ingles6="'table'"
+
+print("El ingles de la palabra "+palabra_esp6+" es "+ingles6)
+
+#ejercicio 11
+palabra_esp7="'Academia'"
+ingles7="'Academy'"
+
+print("El ingles de la palabra "+palabra_esp7+" es "+ingles7)
+
+#ejercicio 12
+palabra_esp8="'Entrada'"
+ingles8="'Input'"
+
+print("El ingles de la palabra "+palabra_esp8+" es "+ingles8)
+
+#ejercicio 13
+palabra_esp9="'Salida'"
+ingles9="'Output'"
+
+print("El ingles de la palabra "+palabra_esp9+" es "+ingles9)
+
+#ejercicio 14
+palabra_esp10="'Impresion'"
+ingles10="'Print'"
+
+print("El ingles de la palabra "+palabra_esp10+" es "+ingles10)
+
+#ejercicio 15
+palabra_esp11="'Cadena'"
+ingles11="'String'"
+
+print("El ingles de la palabra "+palabra_esp11+" es "+ingles11)
